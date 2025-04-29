f4d3407bf6949fd0f6bd4c29e4f687dcd5bce39a
ChennuriSravani <sravanichennuri322@gmail.com>
2025-04-29 18:29:58 +0530

    Add files via upload

---
+++ print.py
@@ -0,0 +1,18 @@
+"""
+a=10
+print(typre(a))
+x=10+3j
+print(type(x))
+"""
+#name=input("Enter your name")
+#print("hello!",name)
+"""
+x=int(input("enter a number"))
+y=int(input("enter a number"))
+sum=x+y
+print("sum=",sum)      
+"""
+"""
+
+
+
